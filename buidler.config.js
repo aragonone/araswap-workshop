28ecbbdcc09c249a4a9dff6839b96d0d79d783a6
0xGabi <gabrielpk.18@gmail.com>
2020-05-29 12:56:58 -0300

    Add etherscan config and update arapp.jon to open registry

--- buidler.config.js
+++ buidler.config.js
@@ -24,6 +24,14 @@ module.exports = {
   gasReporter: {
     enabled: !!process.env.GAS_REPORTER,
   },
+  // Etherscan plugin configuration. Learn more at https://github.com/nomiclabs/buidler/tree/master/packages/buidler-etherscan
+  etherscan: {
+    // The url for the Etherscan API you want to use.
+    url: 'https://api-rinkeby.etherscan.io/api',
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.API_KEY,
+  },
   // Aragon plugin configuration
   aragon: {
     appServePort: 8001,
